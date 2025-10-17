a3cd0fb5852b84aadb7dd5a66ee09d1141af9fa0
drodriguez162 <drodriguez@tiendainglesa.com.uy>
2025-10-16 21:50:25 -0300

    fix - compania

--- Entidades/Compania.py
+++ Entidades/Compania.py
@@ -1,5 +1,29 @@
 class Compania:
-    lista_companias = ["pluna", "vali", "utl"]
+    def __init__(self,nombre, pais, codigo):
+        self.__nombre = nombre
+        self.__pais = pais
+        self.__codigo = codigo
+        
+        @property
+        def nombre(self):
+            return self.__nombre
+        @nombre.setter
+        def nombre(self, nombre): 
+            self.__nombre = nombre
+
+        @property
+        def pais(self): 
+            return self.__pais
+        @pais.setter
+        def pais(self, pais): 
+            self.__pais = pais
+
+        @property
+        def codigo(self):
+            return self.__codigo
+        @codigo.setter
+        def codigo(self, codigo): 
+            self.__codigo = codigo
 
     def validar_compania(self, compania):  #  ahora recibe el parámetro
         if compania in Compania.lista_companias:
