968a83140c5d3e7c2d112961437310a887945b07
MathiasArenas <mathiasarenas@gmail.com>
2025-10-18 22:42:13 -0300

    ajuste registro

--- Entidades/Cliente.py
+++ Entidades/Cliente.py
@@ -6,6 +6,13 @@ class Cliente(Persona):
         self.__nacionalidad = nacionalidad
         self.__historial_vuelos = historial_vuelos
     def registrar_persona(self):
+        nombre = input ("Ingrese el nombre del cliente: ")
+        apellido = input ("Ingrese el apellido del cliente: ")
+        documentoId = input ("Ingrese el documento de identidad del cliente: ")
+        email = input ("Ingrese el email del cliente: ")
+        celular = input ("Ingrese el celular del cliente: ")
+        nacionalidad = input ("Ingrese la nacionalidad del cliente: ")
+        historial_vuelos = input ("Ingrese el historial de vuelos del cliente: ")
         return ("Cliente registrado", self.nombre, self.apellido, self.documentoId, self.email, self.celular, self.__nacionalidad, self.__historial_vuelos)
 
 
