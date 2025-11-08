957b999589fd8dcc4478ffe89798937349c39374
drodriguez162 <drodriguez@tiendainglesa.com.uy>
2025-11-07 23:29:29 -0300

    comienzo con cancelar ticket

--- Entidades/ticket.py
+++ Entidades/ticket.py
@@ -1,3 +1,5 @@
+from entidades.vuelos import Vuelos
+
 class Ticket:
     def __init__(self, id_ticket, cliente, estado, vuelo):
         self.__id_ticket = id_ticket
@@ -39,3 +41,16 @@ class Ticket:
 
     def crear_ticket(self):
         pass
+
+    def cancelar_ticket(self,lista_vuelos):
+        vuelo = Vuelos.buscar_vuelo_por_id(lista_vuelos)
+        id_ticket = input("Ingrese ticket a cancelar: ")
+
+        for ticket in vuelo.__clientes:
+            if ticket.__id_ticket.upper() == id_ticket.upper():
+                ticket.__estado = "Cancelado"
+                print("Ticket cancelado con exito.")
+                return ticket
+            
+    def agregat_en_tickets_cancelados(self, lista_tickets_cancelados):
+        lista_tickets_cancelados.append(self)
